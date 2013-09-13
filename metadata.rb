8f99f23c97f8d91ec7b39baeb5ab1a1c47e55dee
futoase <futoase@gmail.com>
2013-09-13 14:35:20 +0900

    Modified: Repair version.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@aiming-inc.com'
 license          'MIT License.'
 description      'Installs/Configures growthforecast'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '0.1.0'
 
 depends          'perlbrew'
 depends          'mysql'
